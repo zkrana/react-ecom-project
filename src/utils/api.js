b2f9e2c0cb4a50077e9603b432dff5b72e7e2696
Zk Rana <zkranao@gmail.com>
2023-09-01 08:40:10 +0600

    Strapi API key added and Dev url added

--- src/utils/api.js
+++ src/utils/api.js
@@ -19,9 +19,9 @@ export const fetchDataFromApi = async (url) => {
     }
 };
 
-export const makePaymentRequest = axios.create({
-    baseURL: process.env.REACT_APP_STRIPE_APP_DEV_URL,
-    headers: {
-        Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
-    },
-});
+// export const makePaymentRequest = axios.create({
+//     baseURL: process.env.REACT_APP_STRIPE_APP_DEV_URL,
+//     headers: {
+//         Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
+//     },
+// });
